25f78144614753a8980e34daebee460e0530aa1c
John Fulton <fulton@redhat.com>
2016-08-18 10:35:33 -0400

    update heat_debug.sh do not only grep for 0 resources and add new-line to Try to make copy/paste easier.

--- heat_debug.sh
+++ heat_debug.sh
@@ -2,7 +2,7 @@
 # Filename:                heat_debug.sh
 # Description:             Gathers Heat Debug info
 # Supported Langauge(s):   GNU Bash 4.2.x and Heat 0.8.0
-# Time-stamp:              <2016-02-04 10:30:52 jfulton> 
+# Time-stamp:              <2016-08-18 10:34:22 jfulton> 
 # -------------------------------------------------------
 # Gets heat deployment-show data as described in: 
 #  http://hardysteven.blogspot.com/2015/04/debugging-tripleo-heat-templates.html
@@ -10,7 +10,8 @@
 # Commits horrible string hacks to unpack the embedded error
 # into a log file to make debugging a little easier. 
 # -------------------------------------------------------
-heat resource-list --nested-depth 5 overcloud | grep FAILED | grep " 0 " | awk {'print $4'} > /tmp/failed_heat_zero_ids 
+#heat resource-list --nested-depth 5 overcloud | grep FAILED | grep " 0 " | awk {'print $4'} > /tmp/failed_heat_zero_ids 
+heat resource-list --nested-depth 5 overcloud | grep FAILED | awk {'print $4'} > /tmp/failed_heat_zero_ids
 for id in `cat /tmp/failed_heat_zero_ids`; do 
     heat deployment-show $id > /tmp/heat-$id
     echo "Saved a 'heat deployment-show' output to /tmp/heat-$id"
@@ -24,5 +25,5 @@ for id in `cat /tmp/failed_heat_zero_ids`; do
     echo "print d['deploy_stdout']" >> $py_hack
     # ... uggggghhh ....
     python $py_hack | sed -e s/'u001b'//g -e s/'\\/'/g -e s/'\[0m'//g -e s/'\[m'//g  > /tmp/heat-$id.log 
-    echo "Try: cat /tmp/heat-$id.log | ccze -A | less -R" 
+    echo -e "Try: \n\tcat /tmp/heat-$id.log | ccze -A | less -R" 
 done 

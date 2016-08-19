7d8ec9a5f8c438bc13b425a35cd5d1de3b025283
John Fulton <fulton@redhat.com>
2016-08-19 17:56:53 +0000

    update git-init.sh to install git review if it is missing; install is done by downloading the and installing git-review RPM from EPEL but NOT enabling the epel repo

--- ooo-ci-wrap/git-init.sh
+++ ooo-ci-wrap/git-init.sh
@@ -2,7 +2,7 @@
 # Filename:                git-init.sh
 # Description:             configures my git env
 # Supported Langauge(s):   GNU Bash 4.2.x
-# Time-stamp:              <2016-08-19 15:13:33 stack> 
+# Time-stamp:              <2016-08-19 17:51:02 stack> 
 # -------------------------------------------------------
 # Clones the repos that I am interested in.
 # -------------------------------------------------------
@@ -18,6 +18,19 @@ git config --global user.email "fulton@redhat.com"
 git config --global user.name "John Fulton"
 git config --global push.default simple
 git config --global gitreview.username $gerrit_user
+# -------------------------------------------------------
+git review --version
+if [ $? -gt 0 ]; then
+    echo "installing git-review from upstream"
+    dir=/tmp/$(date | md5sum | awk {'print $1'})
+    mkdir $dir
+    pushd $dir
+    curl -O ftp://195.220.108.108/linux/epel/7/x86_64/g/git-review-1.24-5.el7.noarch.rpm
+    sudo yum localinstall git-review-1.24-5.el7.noarch.rpm -y 
+    popd 
+    rm -rf $dir
+fi 
+# -------------------------------------------------------
 pushd ~
 for repo in "${repos[@]}"; do
     dir=$(echo $repo | awk 'BEGIN { FS = "/" } ; { print $2 }')

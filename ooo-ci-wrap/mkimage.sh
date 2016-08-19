b22434a7107d39d46d9ba84dd7bb17d2812ada7d
John Fulton <fulton@redhat.com>
2016-08-19 19:37:58 +0000

    update mkimage.sh to have cleaning option

--- ooo-ci-wrap/mkimage.sh
+++ ooo-ci-wrap/mkimage.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # Filename:                mkimage.sh
-# Description:             wrapper to triploe.sh
+# Description:             wrapper to tripleo.sh
 # Supported Langauge(s):   GNU Bash 4.2.x
-# Time-stamp:              <2016-08-19 17:22:34 stack> 
+# Time-stamp:              <2016-08-19 19:05:48 stack> 
 # -------------------------------------------------------
+CLEAN=1
 export NODE_DIST=centos7
 export USE_DELOREAN_TRUNK=1
 export DELOREAN_TRUNK_REPO="http://buildlogs.centos.org/centos/7/cloud/x86_64/rdo-trunk-master-tripleo/"
@@ -15,6 +16,13 @@ export OS_PASSWORD=Redhat01
 if [[ ! -f $DIB_LOCAL_IMAGE ]]; then
     echo "$DIB_LOCAL_IMAGE does not exist. Exiting."
     exit 1
+fi
+
+if [ $CLEAN -eq 1 ]; then
+    sudo rm -f ~/overcloud-full.{vmlinuz,initrd,qcow2}
+    sudo rm -f ~/ironic-python-agent.{vmlinuz,initrd,qcow2}
+    sudo rm -f /httpboot/agent.kernel
+    sudo rm -f /httpboot/agent.ramdisk
 fi
 
 ~/tripleo-ci/scripts/tripleo.sh --repo-setup
